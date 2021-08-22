review and fix typos; add missing right edge computation

agf/decimate.py
@@ -50,55 +50,59 @@ def decimate(arr: np.ndarray,
 def _decimate_once(arr: np.ndarray, k: int, m: int) -> np.ndarray:
     """decimate a (k*m) x (k*m) array made of k x k blocks"""
 
-    # upper left edge case (i = 0)
-    # get refs. to initial values
+    # upper left edge (i = 0)
+    # -----------------------
     W_00, iW = get_block(arr, k, 0, 0)
-    t_01, i1 = get_block(arr, k, 0, 1)
+    t_0_0p, i1 = get_block(arr, k, 0, 1)
 
-    # adjacent blocks for updating values
-    W_11 = get_block(arr, k, 1, 1)[0]
-    t_10 = get_block(arr, k, 1, 0)[0]
-    t_12 = get_block(arr, k, 1, 2)[0]
+    t_0p_0 = get_block(arr, k, 1, 0)[0]
+    t_0p_0pp = get_block(arr, k, 1, 2)[0]
+    W_0p_0p = get_block(arr, k, 1, 1)[0]
 
-    # compute inversions
-    g_11 = np.linalg.inv(W_11)
-
-    # update values at refs.
-    arr[iW[0]:iW[1], iW[2]:iW[3]] = W_00 - t_01 @ g_11 @ t_10  # W_00
-    arr[i1[0]:i1[1], i1[2]:i1[3]] = t_01 @ -g_11 @ t_12  # t_01
+    g_11 = np.linalg.inv(W_0p_0p)
+    arr[iW[0]:iW[1], iW[2]:iW[3]] = W_00 - t_0_0p @ g_11 @ t_0p_0  # new W_00
+    arr[i1[0]:i1[1], i1[2]:i1[3]] = -t_0_0p @ g_11 @ t_0p_0pp  # new t_0_0p
 
     # repeat for interior blocks
+    # --------------------------
     for i in range(1, m - 1):
         # indices name legend:
         # im -> i-1
         # ip -> i+1
         # imm -> i-2
         # ipp -> i+2
-
-        # principal blocks at this iteration
         W_ii, iW = get_block(arr, k, i, i)
-        t_im_i, i0 = get_block(arr, k, i - 1, i)
-        t_ip_i, i1 = get_block(arr, k, i + 1, i)
+        t_i_im, i0 = get_block(arr, k, i, i - 1)
+        t_i_ip, i1 = get_block(arr, k, i, i + 1)
 
-        # adjacent blocks for updating values
         W_im_im = get_block(arr, k, i - 1, i - 1)[0]
         W_ip_ip = get_block(arr, k, i + 1, i + 1)[0]
-
-        t_i_im = get_block(arr, k, i, i - 1)[0]
-        t_i_ip = get_block(arr, k, i, i + 1)[0]
-
-        t_ip_ipp = get_block(arr, k, i + 1, i + 2)[0]
+        t_im_i = get_block(arr, k, i - 1, i)[0]
+        t_ip_i = get_block(arr, k, i + 1, i)[0]
         t_im_imm = get_block(arr, k, i - 1, i - 2)[0]
+        t_ip_ipp = get_block(arr, k, i + 1, i + 2)[0]
 
-        # compute inversions
-        g_ip_ip = np.linalg.inv(W_ip_ip)
         g_im_im = np.linalg.inv(W_im_im)
-
-        # update values at refs.
-        arr[iW[0]:iW[1], iW[2]:iW[3]] = (  # new W_ii
-                W_ii - t_i_im @ g_im_im @ t_im_i + t_i_ip @ g_ip_ip @ t_ip_i
+        g_ip_ip = np.linalg.inv(W_ip_ip)
+        arr[iW[0]:iW[1], iW[2]:iW[3]] = (
+                W_ii - t_i_im @ g_im_im @ t_im_i - t_i_ip @ g_ip_ip @ t_ip_i  # new W_ii
         )
-        arr[i0[0]:i0[1], i0[2]:i0[3]] = -t_i_im @ g_im_im @ t_im_imm  # new t_im_i
-        arr[i1[0]:i1[1], i1[2]:i1[3]] = -t_i_ip @ g_ip_ip @ t_ip_ipp  # new t_ip_i
+        arr[i0[0]:i0[1], i0[2]:i0[3]] = -t_i_im @ g_im_im @ t_im_imm
+        arr[i1[0]:i1[1], i1[2]:i1[3]] = -t_i_ip @ g_ip_ip @ t_ip_ipp
+        print(arr)
+
+    # lower right edge (i = m - 1 = f)
+    # --------------------------------
+    f = m - 1
+    W_ff, iW = get_block(arr, k, f, f)
+    t_f_fm, i0 = get_block(arr, k, f, f - 1)
+
+    W_fm_fm = get_block(arr, k, f - 1, f - 1)[0]
+    t_fm_f = get_block(arr, k, f - 1, f)[0]
+    t_fm_fmm = get_block(arr, k, f - 1, f - 2)[0]
+
+    g_fm_fm = np.linalg.inv(W_fm_fm)
+    arr[iW[0]:iW[1], iW[2]:iW[3]] = W_ff - t_f_fm @ g_fm_fm @ t_fm_f
+    arr[i0[0]:i0[1], i0[2]:i0[3]] = -t_f_fm @ g_fm_fm @ t_fm_fmm
 
     return arr