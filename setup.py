d2b90216ba04b92d4e0ad35c8b2011c9192599cc
Ara Ghukasyan <ghukasa@mcmaster.ca>
2021-09-07 19:14:45 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+from agf import __version__
+
+setup(
+    name="agf",
+    version=__version__,
+    description="A package for computing Atomistic Green's Functions based on the Zhang-Mingo method.",
+    author="Ara Ghukasyan",
+    install_requires=["numpy", "pandas"],
+    packages=find_packages()
+)
