50e719ed93259668834b81c685cf9d88c613c9e5
Ara Ghukasyan <ghukasa@mcmaster.ca>
2021-09-07 14:21:41 -0400

    simplest nanowire working

--- tests/simplest_nw.py
+++ tests/simplest_nw.py
@@ -2,10 +2,23 @@ import numpy as np
 from agf import Section, get_solver, get_zhang_delta
 
 
-def test():
-    atom_positions_file = "~/Desktop/agf_tests/RELAXED_simplest_device.data"
+def main():
+    from my_phonolammps.phonon_runner import PhononRunner
+
+    wire_datafile = "~/Desktop/agf_tests/zstack_test.data"
+    potential_file = "~/Desktop/agf_tests/GaAs.tersoff"
+    ph = PhononRunner(wire_datafile,
+                      potential_file,
+                      output_dir="~/Desktop/agf_tests",
+                      generic_in="~/Desktop/agf_tests/generic_in.lammps",
+                      generic_relax="~/Desktop/agf_tests/generic_relax.lammps",
+                      marker="simple_nanowire")
+    ph.relax_structure()
+    ph.compute_forces()
+
+    atom_relaxed_positions_file = "~/Desktop/agf_tests/RELAXED_simple_nanowire.data"
     layer_map_file = "~/Desktop/agf_tests/zstack_test.map"
-    force_constants_file = "~/Desktop/agf_tests/FC_simplest_device"
+    force_constants_file = "~/Desktop/agf_tests/FC_simple_nanowire"
     layer_assignments = {
         Section.LCB: [0, 1],
         Section.LC: 2,
@@ -14,26 +27,34 @@ def test():
         Section.RCB: [5, 6]
     }
 
-    agf = get_solver(atom_positions_file,
+    agf = get_solver(atom_relaxed_positions_file,
                      layer_map_file,
                      force_constants_file,
                      layer_assignments)
 
-    omegas = np.linspace(10, 10e13)
+    omegas = np.linspace(1, 6, 400)
     deltas = get_zhang_delta(omegas)
     trans = []
+    dos = []
     i = 1
     n_omega = len(omegas)
     for omega, delta in zip(omegas, deltas):
         print(f"frequency {i}/{n_omega}")
         result = agf.compute(omega, delta)
-        trans.append(result.transmission)
+        t = result.transmission
+        trans.append(t)
+
+        d = result.dos[0][0].real
+        dos.append(d)
+
         i += 1
 
     import matplotlib.pyplot as plt
     plt.plot(omegas, trans)
     plt.show()
+    plt.plot(omegas**2, dos)
+    plt.show()
 
 
 if __name__ == "__main__":
-    test()
+    main()
