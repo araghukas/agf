d01a7e01231cb5a8e41d14135ef045b5fd33bc18
Ara Ghukasyan <ghukasa@mcmaster.ca>
2021-09-16 16:55:39 -0400

    write output while computing transmission

--- agf/__init__.py
+++ agf/__init__.py
@@ -1,12 +1,13 @@
 from typing import Dict, Union, Sequence, Callable
 from numpy import ndarray
+from os.path import expanduser
 import warnings
 
 from agf.structure import Section
 from agf.agf import AGF
 from agf.hm import HarmonicMatrix
 
-__version__ = "2021.3"
+__version__ = "2021.3.1"
 
 
 def disable_log():
@@ -74,7 +75,6 @@ def compute_transmission(omegas: Sequence[float],
                          log_progress: bool = True,
                          delta_func: Callable[[float], float] = None,
                          delta_func_kwargs: dict = None,
-                         save_results: bool = True,
                          results_savename: str = None) -> ndarray:
     """
     Compute the transmission over several angular frequencies and return the result.
@@ -91,7 +91,6 @@ def compute_transmission(omegas: Sequence[float],
     :param log_progress: print AGF progress messages
     :param delta_func: function that returns broadening at each frequency
     :param delta_func_kwargs: dictionary of keyword arguments passed to `delta_func`
-    :param save_results: save the results to a CSV file
     :param results_savename: name of the results file
 
     :return: array of transmission values
@@ -115,16 +114,11 @@ def compute_transmission(omegas: Sequence[float],
     elif delta_func_kwargs is None:
         delta_func_kwargs = {}
 
-    if not save_results:
-        if results_savename:
-            warnings.warn("results won't be saved, pass 'save_results=True' to save.")
-
     if not results_savename:
-        if save_results:
-            results_savename = "transmission_.csv"
-            warnings.warn(
-                f"results will be saved under default name '{results_savename}'."
-            )
+        results_savename = "transmission_.csv"
+        warnings.warn(
+            f"results will be saved under default name '{results_savename}'."
+        )
 
     n_omegas = len(omegas)
 
@@ -132,30 +126,17 @@ def compute_transmission(omegas: Sequence[float],
     deltas = delta_func(omegas, **delta_func_kwargs)
     trans = zeros(n_omegas)
 
-    i_freq = 1
-    AGF.print("\nindex, omega, transmission")
-    for omega, delta in zip(omegas, deltas):
-        result = model.compute(omega, delta)
-        t = result.transmission
-        AGF.print("{:<4,d} {:<12,.6e} {:<12,.6e}".format(i_freq-1, omega, t))
-        trans[i_freq - 1] = t
-        i_freq += 1
-
-    if save_results:
-        try:
-            from pandas import DataFrame
-            df = DataFrame({
-                "omega": omegas,
-                "transmission": trans
-            })
-            df.to_csv(results_savename)
-        except ModuleNotFoundError:
-            with open(results_savename, 'w') as f:
-                f.write(",omega,transmission\n")
-                i = 0
-                for omega, t in zip(omegas, trans):
-                    f.write("{:d},{:f},{:f}\n".format(i, omega, t))
-                    i += 1
-                f.write("\n")
+    i = 0
+    with open(expanduser(results_savename), 'w') as output_file:
+        output_file.write(",omega,transmission\n")
+        AGF.print("\nindex, omega, transmission")
+        for omega, delta in zip(omegas, deltas):
+            result = model.compute(omega, delta)
+            tr = result.transmission
+            output_file.write("{:d},{:f},{:f}\n".format(i, omega, tr))
+            AGF.print("{:<4,d} {:<12,.6e} {:<12,.6e}".format(i, omega, tr))
+            trans[i] = tr
+            i += 1
+        output_file.write("\n")
 
     return trans
