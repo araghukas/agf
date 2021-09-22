a6191f3204d676719ccc49e05dee46493ea3a63e
Ara Ghukasyan <ghukasa@mcmaster.ca>
2021-09-22 13:31:39 -0400

    add url and author_email

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ setup(
     name="agf",
     version=__version__,
     description="A package for computing Atomistic Green's Functions based on the Zhang-Mingo method.",
+    url="https://github.com/araghukas/agf.git",
+    author_email="ghukasa@mcmaster.ca",
     author="Ara Ghukasyan",
     install_requires=["numpy", "numba"],
     packages=find_packages()
